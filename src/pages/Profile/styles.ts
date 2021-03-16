91e642bc257300c0fe8ffb9a13bf30c4d1d9b82a
Deivid Alan <deividalan@hotmail.com>
2021-03-15 22:15:54 -0300

    First commit

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 100 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 24px 0px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 60px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 150px;
+  height: 150px;
+  border-radius: 75px;
+
+  align-self: center;
+`;
